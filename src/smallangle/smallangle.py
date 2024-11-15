edd60c910a1a53b7cb747244e189cddcaaa2f16b
TristanBlikman <tristan@blikman.nl>
2024-11-15 14:02:27 +0100

    command toegevoegd + code aanpassing

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,16 +3,29 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-@click.command()
+@click.group()
+def cmd_group():
+    pass
+
+@cmd_group.command()
 @click.argument("number")
+@click.option(    
+    "-n",
+    "--number",
 
-
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
+@cmd_group.command()
+@click.argument("number")
+@click.option(    
+    "-n",
+    "--number",
 
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -21,3 +34,6 @@ def tan(number):
 
 if __name__ == "__main__":
     sin(10)
+
+# if __name__ == "__main__":
+#     cmd_group()
